ce1c01a9dfaf86b565cb87a1e714aa0e54ba4955
arshitrupanii <arshitrupani6@gmail.com>
2025-12-04 20:47:14 +0530

    update

---
+++ x.com/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: 'rgb(29, 155, 240)', // Your custom color
+      }
+    },
+  },
+  plugins: [],
+}
